e6c20c5b85dc5fd65847a63e160f2534879f6e7c
noah milliken <noahmilliken@gmail.com>
2023-01-21 14:55:05 -0500

    API hooked up, getting data, html up and functioning as is CSS

---
+++ index.js
@@ -0,0 +1,39 @@
+const colorPicker = document.getElementById('color')
+const getColorBtn = document.getElementById('get-color-btn')
+
+const mode = document.getElementById('mode-select')
+const selectedMode = mode.value
+
+
+getColorBtn.addEventListener('click', getColor)
+function getColor() {
+
+    console.log(selectedMode)
+    console.log('clicked')
+    console.log(mode)
+}
+// colorPicker.addEventListener('change', function (event) {
+//     let colorSwatch = event.target.value
+//     let noHex = colorSwatch.substring(1)
+//     console.log(noHex)
+//     fetch(`https://www.thecolorapi.com/id?hex=${noHex}`)
+//         .then(res => res.json())
+//         .then(json => console.log(json))
+
+// })
+
+
+
+
+
+
+// let firstCol = document.getElementById('first-column')
+// let secondCol = document.getElementById('second-column')
+// let thirdCol = document.getElementById('third-column')
+// let fourthCol = document.getElementById('fourth-column')
+// let fifthCol = document.getElementById('fifth-column')
+    // firstCol.style.backgroundColor = `${colorSwatch}`
+    // secondCol.style.backgroundColor = `${colorSwatch}`
+    // thirdCol.style.backgroundColor = `${colorSwatch}`
+    // fourthCol.style.backgroundColor = `${colorSwatch}`
+    // fifthCol.style.backgroundColor = `${colorSwatch}`
